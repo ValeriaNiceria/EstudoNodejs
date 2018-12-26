29ef363c4a49b62098d98d1c2aa2dde54a402a59
ValeriaNiceria <valerianiceria@gmail.com>
2018-12-26 11:44:18 -0200

    Criando um service - consumindo API swapi

---
+++ #2 - Manipulação de listas/For-ForIn-ForOf/service.js
@@ -0,0 +1,17 @@
+// Consumindo os dados da API - 'https://swapi.co/'
+// npm install axios
+
+const axios = require('axios')
+const URL = `https://swapi.co/api/people`
+
+async function obterPessoas(nome) {
+    const url = `${ URL }/?search=${nome}&format=json`
+    const response = await axios.get(url)
+    return response.data
+}
+
+// obterPessoas('r2').then((resultado) => {
+//     console.log('Resultado: ', resultado)
+// }).catch((error) => {
+//     console.error('Deu ruim: ', error)
+// })
