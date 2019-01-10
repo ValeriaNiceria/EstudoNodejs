4fc3e2f0dc4a6d7fef5d9a41f071abadadaf500c
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-09 23:05:44 -0200

    usando o pacote 'nock' para simular requi requisições

--- #3 - Introdução a desenvolvimento de teste automatizados/01/service.js
+++ #3 - Introdução a desenvolvimento de teste automatizados/01/service.js
@@ -5,7 +5,8 @@ const URL = `https://swapi.co/api/people`
 async function obterPessoas(nome) {
 	const url = `${URL}/?search=${nome}&format=json`
 	const result = await get(url)
-	return result.data.results.map(mapearPessoas)
+	// console.log(JSON.stringify(result.data))
+	return result.data.results.map( mapearPessoas )
 }
 
 function mapearPessoas(item) {
