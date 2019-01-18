1a9b08b4c43e2e859a579649866d3a3769739760
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-18 09:35:08 -0200

    Implementando um recurso estático

--- API REST/projetoPiloto/index.js
+++ API REST/projetoPiloto/index.js
@@ -2,5 +2,18 @@ const express = require("express");
 const PORT = 3000;
 
 const app = express();
+
+app.set("json spaces", 4);
+
 app.get("/", (req, res) => res.json({status: "Projeto Piloto API"}));
+
+app.get("/task", (req, res) => {
+	res.json({
+		tasks: [
+			{title: "Fazer compras"},
+			{title: "Consertar o pc"}
+		]
+	});
+});
+
 app.listen(PORT, () => console.log(`Projeto Piloto API = porta ${PORT}`));
