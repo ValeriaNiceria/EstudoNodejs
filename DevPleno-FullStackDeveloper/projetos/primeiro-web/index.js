5e5d8849e08da75b5293d0cdca31e0a10f5be3a2
ValeriaNiceria <valerianiceria@gmail.com>
2019-02-02 21:35:03 -0200

    usando session

--- DevPleno-FullStackDeveloper/projetos/primeiro-web/index.js
+++ DevPleno-FullStackDeveloper/projetos/primeiro-web/index.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
 
-const PORT = 3000
+app.use(session({
+    secret: 'fullstackacademy',
+    resave: true,
+    saveUninitialized: true
+}))
 
-let count = 0
+
 app.get('/', (req, res) => {
-    res.send(`<h1>Olá - ${ ++count }</h1>`)
+    let count = 0
+    if (req.session.count) {
+        count = req.session.count
+    }
+    ++count
+    req.session.count = count
+    res.send(`<h1>Olá - ${ count }</h1>`)
 })
 
 app.get('/page1', (req, res) => {
+    let count = 0
+    if (req.session.count) {
+        count = req.session.count
+    }
+    ++count
+    req.session.count = count
     res.send(`<h1>Hello - ${ count }</h1>`)
 })
 
-
+const PORT = 3000
 app.listen(PORT, err => {
     if (err) {
         console.log('Não foi possível iniciar')
