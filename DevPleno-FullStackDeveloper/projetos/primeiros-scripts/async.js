d22d0a5ef61fd54bb128cda8747b3f295b2b64d3
ValeriaNiceria <valerianiceria@gmail.com>
2019-02-02 19:27:45 -0200

    async

---
+++ DevPleno-FullStackDeveloper/projetos/primeiros-scripts/async.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+const lerArquivo = arquivo => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(arquivo, (err, conteudo) => {
+            if (err) {
+                reject('Não foi possível ler o arquivo', err)
+            } else {
+                resolve(conteudo)
+            }
+        })
+    })
+}
+
+// lerArquivo('01.js')
+//     .then((conteudo) => {
+//         console.log(String(conteudo))
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     })
+
+const executa = async() => {
+    const conteudo = await lerArquivo('01.js')
+    console.log(String(conteudo))
+}
+
+executa()
+    .then(() => {
+        console.log('TERMINOU!')
+    })
